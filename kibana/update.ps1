11dd7b95c36ebc10acb272362634fa81651071cc
Chris Magnuson <hourglasssand@gmail.com>
2017-01-11 17:36:06 -0500

    Initial commit of Kibana in au style

---
+++ kibana/update.ps1
@@ -0,0 +1,27 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    
+    $download_page = Invoke-WebRequest -Uri "https://www.elastic.co/downloads/kibana"
+
+    #kibana-5.1.1-windows-x86.zip
+    $regex  = "kibana-.+-windows-x86.zip"
+    $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
+
+    $version = $url -split '-' | select -First 1 -Skip 1
+    $url32 = $url
+
+    $Latest = @{ URL32 = $url32; Version = $version }
+    return $Latest
+}
+
+update
