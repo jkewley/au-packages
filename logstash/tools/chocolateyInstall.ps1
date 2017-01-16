3752dbc0d1b5890fe2a469a72dd0c07030a25c3c
TervisIT <chocolatey@tervis.com>
2017-01-16 16:27:33 +0000

    AU: 1 updated - logstash [skip ci] https://gist.github.com/909b5f40614cd00c4b237ef30546976d/e36af34c6b9109703b294d7e6866c6f27adaaf2c

--- logstash/tools/chocolateyInstall.ps1
+++ logstash/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'logstash'
-$url32       = 'https://camunda.org/release/camunda-modeler/1.6.0/camunda-modeler-1.6.0-win32-ia32.zip'
-$checksum32  = '1efb2e9e543e8da2938b14dfa5c41b417d62e7fee43f21d1d2bc1a8b706a48ae'
+$url32       = 'https://artifacts.elastic.co/downloads/logstash/logstash-5.1.2.zip'
+$checksum32  = '99fd514b6241310c78aefca2cd895c1cec4cd426aa08fccf7e8bba26567573c3'
 
 $packageArgs = @{
   packageName    = $packageName
