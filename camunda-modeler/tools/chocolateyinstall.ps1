e6ac07dd12cb7e399182cd2db4367bccdb6277c5
TervisIT <chocolatey@tervis.com>
2017-03-21 00:05:25 +0000

    AU: 1 updated - camunda-modeler [skip ci] https://gist.github.com/958bf060c4b5b20935beba56eeebe1ae/0b6a38a1415e7b67e51b4511136c03f0fda239ba

--- camunda-modeler/tools/chocolateyinstall.ps1
+++ camunda-modeler/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'camunda-modeler'
-$url32       = 'https://camunda.org/release/camunda-modeler/1.7.1/camunda-modeler-1.7.1-win32-ia32.zip'
-$url64       = 'https://camunda.org/release/camunda-modeler/1.7.1/camunda-modeler-1.7.1-win32-x64.zip'
-$checksum32  = '26dc0acca44bc735818b044b6a7c9a4a68681b76b9bd7ae9aecc77f8b7adf12d'
-$checksum64  = 'efb94faa597844f11eeaa07689fa4126c716a6bb597df83673222179127e8eb1'
+$url32       = 'https://camunda.org/release/camunda-modeler/1.7.2/camunda-modeler-1.7.2-win32-ia32.zip'
+$url64       = 'https://camunda.org/release/camunda-modeler/1.7.2/camunda-modeler-1.7.2-win32-x64.zip'
+$checksum32  = '2b93a937556d97c1b8df6da5cf77f014a01dc90eed58bd478e3819f0bba48e67'
+$checksum64  = '8f8ae32c5aaa2516f74a8942bd84c2292d7af5fbee0673874b43f41fece600d7'
 
 $packageArgs = @{
   packageName    = $packageName
