2234e745e242b8d41994889df82fc82522ea9a79
TervisIT <chocolatey@tervis.com>
2017-06-14 00:06:10 +0000

    AU: 1 updated - prometheus [skip ci] https://gist.github.com/958bf060c4b5b20935beba56eeebe1ae/e14ffed5c24a671c4fcf72be26dcc8821e596076

--- prometheus/tools/chocolateyinstall.ps1
+++ prometheus/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'prometheus'
-$url32       = 'https://github.com/prometheus/prometheus/releases/download/v1.7.0/prometheus-1.7.0.windows-386.tar.gz'
-$url64       = 'https://github.com/prometheus/prometheus/releases/download/v1.7.0/prometheus-1.7.0.windows-amd64.tar.gz'
-$checksum32  = 'd5cf76b4038e7ee255f296779f3f26423a0ce1212ea3c4e3f98103578f70bcc8'
-$checksum64  = '99d390645fc1abbc36aaa7837852e8380fb0bf1559a3b18a92120d80e015e0e3'
+$url32       = 'https://github.com/prometheus/prometheus/releases/download/v1.7.1/prometheus-1.7.1.windows-386.tar.gz'
+$url64       = 'https://github.com/prometheus/prometheus/releases/download/v1.7.1/prometheus-1.7.1.windows-amd64.tar.gz'
+$checksum32  = '0b2dc68b2a4209554d3ce0840b0603e2b2a083425ac900410f7e8a635a66dd43'
+$checksum64  = 'c114b760016f1184afb0b92a0b513e32f8cae12d965df09c3f0e4ad32df99dbc'
 
 $packageArgs = @{
   packageName    = $packageName
