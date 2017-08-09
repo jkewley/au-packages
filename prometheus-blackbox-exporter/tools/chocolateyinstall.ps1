d9b8c851bf5652204f53a56e21132c650352b2c2
TervisIT <chocolatey@tervis.com>
2017-08-09 00:03:49 +0000

    AU: 1 updated - prometheus-blackbox-exporter [skip ci] https://gist.github.com/958bf060c4b5b20935beba56eeebe1ae/0fd481db6c558efc7a857cf243d22d5de97f3236

--- prometheus-blackbox-exporter/tools/chocolateyinstall.ps1
+++ prometheus-blackbox-exporter/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $PackageName = 'prometheus-blackbox-exporter'
-$url32       = 'https://github.com/prometheus/blackbox_exporter/releases/download/v0.7.0/blackbox_exporter-0.7.0.windows-386.tar.gz'
-$url64       = 'https://github.com/prometheus/blackbox_exporter/releases/download/v0.7.0/blackbox_exporter-0.7.0.windows-amd64.tar.gz'
-$checksum32  = 'aca6bff76d58afcb3902537b02427d9cb94caf1ba30328246b55a1bb62640908'
-$checksum64  = 'c78a48afd69ad80b5c85ed1d546c652d1dd062664b712b2ef6f468df8806ac6a'
+$url32       = 'https://github.com/prometheus/blackbox_exporter/releases/download/v0.8.1/blackbox_exporter-0.8.1.windows-386.tar.gz'
+$url64       = 'https://github.com/prometheus/blackbox_exporter/releases/download/v0.8.1/blackbox_exporter-0.8.1.windows-amd64.tar.gz'
+$checksum32  = '716fd78bd05b2fb2706ef6a58fc85468011394791efa076d997619c0863954e0'
+$checksum64  = 'e8f8e0cefbb0bd3aba695cb1b2c34de989b0ac259f9e7fd1989a73dc3c15b875'
 
 $packageArgs = @{
   packageName    = $packageName
