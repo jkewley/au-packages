00e5ee0079d9045dfdd267ed57385b5c9102fdcb
ChrisMagnuson <hourglasssand@gmail.com>
2017-11-14 16:01:59 -0500

    Initial commit of google-cloud-print-connector

---
+++ google-cloud-print-connector/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$PackageName = 'google-cloud-print-connector'
+$url32       = 'https://github.com//google/cloud-print-connector/releases/download/v1.13/windows-connector-1.13.msi'
+$checksum32  = ''
+
+$packageArgs = @{
+  packageName   = $PackageName
+  fileType      = 'msi'
+  url           = $url32
+  silentArgs    = "/qr /i$(if($env:chocolateyPackageParameters){" $env:chocolateyPackageParameters"})"
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ google-cloud-print-connector/update.ps1
@@ -0,0 +1,28 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    
+    $DownloadPage = Invoke-WebRequest -Uri "https://github.com/google/cloud-print-connector/releases"
+
+    #windows-connector-1.13.msi
+    $regex  = "windows-connector-.+.msi"
+    $url = $DownloadPage.links | ? href -match $regex | select -First 1 -expand href
+
+    $VersionWithExtension = $url -split "-" | select -Last 1
+    $Version = $VersionWithExtension -split "\.msi" | select -First 1
+    $url32 = "https://github.com/$url"
+
+    $Latest = @{ URL32 = $url32; Version = $version }
+    return $Latest
+}
+
+update
