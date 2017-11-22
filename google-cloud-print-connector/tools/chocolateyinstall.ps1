8464f8a0a2ddb30266cfdd3f520dc5f4788b9c28
ChrisMagnuson <hourglasssand@gmail.com>
2017-11-22 17:08:49 -0500

    Update of google cloud print connector

--- google-cloud-print-connector/tools/chocolateyinstall.ps1
+++ google-cloud-print-connector/tools/chocolateyinstall.ps1
@@ -2,13 +2,13 @@
 
 $PackageName = 'google-cloud-print-connector'
 $url32       = 'https://github.com//google/cloud-print-connector/releases/download/v1.13/windows-connector-1.13.msi'
-$checksum32  = ''
+$checksum32  = '93672ee361db7e6f6baa6b5db6578169ff707b3ce856d476e5662a8629f83fd2'
 
 $packageArgs = @{
   packageName   = $PackageName
   fileType      = 'msi'
   url           = $url32
-  silentArgs    = "/qr /i$(if($env:chocolateyPackageParameters){" $env:chocolateyPackageParameters"})"
+  silentArgs    = "/qn /i$(if($env:chocolateyPackageParameters){" $env:chocolateyPackageParameters"})"
   checksum      = $checksum32
   checksumType  = 'sha256'
 }
