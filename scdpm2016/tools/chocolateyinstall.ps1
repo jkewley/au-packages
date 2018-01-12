e958e24c9b9c54c9e89e83c5d47fa7aed7e1c03d
mastermohl <mastermohl@gmail.com>
2018-01-12 11:22:38 -0500

    Added SCDPM2016

---
+++ scdpm2016/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'SCDPM2016'
+$packageVersion = "$Version"
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ConfigFileLocation = $(Join-Path $toolsDir 'SCDPM2016Configuration.txt')
+
+$setupPackageArgs = @{
+  packageName = $packageName
+  fileType = 'EXE'
+  file = '\\tervis.prv\applications\Installers\Microsoft\SCDPM2016\Setup.exe'
+  silentArgs = "/i /f $PackageParameters"
+}
+
+Install-ChocolateyInstallPackage @setupPackageArgs
---
+++ scdpm2016/tools/chocolateyuninstall.ps1
@@ -0,0 +1,10 @@
+﻿$PackageName="prometheus"
+$ServiceName="prometheus-service"
+
+if ($Service = Get-Service $ServiceName -ErrorAction SilentlyContinue) {
+    Write-Host "Removing service."
+    if ($Service.Status -eq "Running") {
+        Start-ChocolateyProcessAsAdmin "stop $ServiceName" "sc.exe"
+    }
+    Start-ChocolateyProcessAsAdmin "delete $ServiceName" "sc.exe"
+}
