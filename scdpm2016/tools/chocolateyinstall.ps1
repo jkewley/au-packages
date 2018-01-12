99b746b64bafafddb7baecbecf850eafb7e67784
mastermohl <mastermohl@gmail.com>
2018-01-12 17:03:47 -0500

    fixed SCDPM2016 install

--- scdpm2016/tools/chocolateyinstall.ps1
+++ scdpm2016/tools/chocolateyinstall.ps1
@@ -4,13 +4,12 @@ $packageName = 'SCDPM2016'
 $packageVersion = "$Version"
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$ConfigFileLocation = $(Join-Path $toolsDir 'SCDPM2016Configuration.txt')
 
 $setupPackageArgs = @{
   packageName = $packageName
-  fileType = 'EXE'
-  file = '\\tervis.prv\applications\Installers\Microsoft\SCDPM2016\Setup.exe'
-  silentArgs = "/i /f $PackageParameters"
+  fileType = "EXE"
+  file = "\\tervis.prv\applications\Installers\Microsoft\SCDPM2016\Setup.exe"
+  silentArgs = "$PackageParameters"
 }
 
 Install-ChocolateyInstallPackage @setupPackageArgs
