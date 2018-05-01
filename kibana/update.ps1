8d12087bdf7452e9a07f6c76f351e19adc4b8859
ChrisMagnuson <hourglasssand@gmail.com>
2018-05-01 12:57:29 -0400

    Fix regex to match Kibana download file name

--- kibana/update.ps1
+++ kibana/update.ps1
@@ -14,7 +14,7 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri "https://www.elastic.co/downloads/kibana" -UseBasicParsing
 
     #kibana-5.1.1-windows-x86.zip
-    $regex  = "kibana-.+-windows-x86.zip"
+    $regex  = "kibana-.+-windows-x86_64.zip"
     $url = $download_page.links | ? href -match $regex | select -First 1 -expand href
 
     $version = $url -split '-' | select -First 1 -Skip 1
